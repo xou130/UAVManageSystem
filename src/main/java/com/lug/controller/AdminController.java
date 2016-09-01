3f9c1c980f425e450169ff2848c5de587a2b96c4
zengzhishi <zudaojun@outlook.com>
2016-09-01 23:32:24 +0800

    Change thymeleaf templates to freemarker, and delete uav class

--- src/main/java/com/lug/controller/AdminController.java
+++ src/main/java/com/lug/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.lug.controller;
 
 import com.lug.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +12,7 @@ import java.util.Map;
  * Created by zzs on 2016/9/1.
  */
 @Controller
+@EnableAutoConfiguration
 @RequestMapping("/admin")
 public class AdminController {
 
@@ -21,9 +23,11 @@ public class AdminController {
         this.adminRepository = adminRepository;
     }
 
-    @RequestMapping("/home")
+    @RequestMapping(value = "/home")
     public String adminHome(Map<String,Object> map){
         return "/admin/home";
     }
+
+
 
 }
